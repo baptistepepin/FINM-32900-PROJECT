c7e98e7d297e92239dda7191f860752d52ea7d5e
Baptiste Pepin <baptistepepin@uchicago.edu>
2024-02-22 00:04:38 -0600

    Pull and Load data from CRPS
    We need the outstanding shares available in CRPS database, and all the useful variables to merge with other tables

---
+++ src/load_crps.py
@@ -0,0 +1,81 @@
+"""
+This module pulls and saves data on fundamentals from CRSP.
+It pulls the number of shares outstanding that we will need later for calculating our ratios.
+"""
+
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
+import wrds
+
+from src import config
+
+DATA_DIR = Path(config.DATA_DIR)
+WRDS_USERNAME = config.WRDS_USERNAME
+START_DATE = config.START_DATE
+END_DATE = config.END_DATE
+
+
+def pull_CRSP(
+        start_date=START_DATE,
+        end_date=END_DATE,
+        wrds_username=WRDS_USERNAME
+):
+    """
+    # TODO: Update docstring
+    Pulls CRSP stock data from a specified start date to end date.
+
+    SQL query to pull data
+    """
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+    query = f"""
+    SELECT 
+        date, permno, permco, cusip, shrout
+    FROM crspq.dsf AS dsf
+    WHERE 
+        date BETWEEN '{start_date}' AND '{end_date}'
+    """
+    db = wrds.Connection(wrds_username=wrds_username)
+    df = db.raw_sql(
+        query, date_cols=["date"]
+    )
+    db.close()
+
+    df["shrout"] = df["shrout"] * 1000
+
+    return df
+
+
+def load_CRSP(
+        data_dir=DATA_DIR,
+        from_cache=True,
+        save_cache=False,
+        start_date=START_DATE,
+        end_date=END_DATE,
+        wrds_username=WRDS_USERNAME
+):
+    """
+    # TODO: Add docstring
+    """
+    if from_cache:
+        file_path = Path(data_dir) / "pulled" / "crsp.parquet"
+        CRSP_daily_stock = pd.read_parquet(file_path)
+
+    else:
+        CRSP_daily_stock = pull_CRSP(start_date=START_DATE, end_date=END_DATE, wrds_username=WRDS_USERNAME)
+
+        if save_cache:
+            file_dir = Path(data_dir) / "pulled"
+            file_dir.mkdir(parents=True, exist_ok=True)
+            CRSP_daily_stock.to_parquet(file_dir / 'crsp.parquet')
+
+    return CRSP_daily_stock
+
+
+if __name__ == "__main__":
+    # Pull and save cache of CRSP data
+    _ = load_CRSP(data_dir=DATA_DIR, from_cache=True, save_cache=True, start_date=START_DATE, end_date=END_DATE,
+                  wrds_username=WRDS_USERNAME)
